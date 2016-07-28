31b08e4cc8257945a08f3139f9d4171bd3c611ed
Kieran Gallagher <kags1995@hotmail.co.uk>
2016-07-28 12:57:24 +0100

    4_12 Transition

---
+++ Rebound_4_12/Source/Rebound_4_12/Rebound_4_12GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Rebound_4_12GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REBOUND_4_12_API ARebound_4_12GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
