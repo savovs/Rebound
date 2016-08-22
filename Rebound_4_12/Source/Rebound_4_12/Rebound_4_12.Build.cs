4b0d5bf21d87d6655e292ac665b12598b5a3992c
amacdiarmid <amacdiarmid@hotmail.co.uk>
2016-08-22 12:32:11 +0100

    networking set up for 4.12

--- Rebound_4_12/Source/Rebound_4_12/Rebound_4_12.Build.cs
+++ Rebound_4_12/Source/Rebound_4_12/Rebound_4_12.Build.cs
@@ -12,15 +12,15 @@ public class Rebound_4_12 : ModuleRules
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
+		PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+		{
+			if (UEBuildConfiguration.bCompileSteamOSS == true)
+			{
+				DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+			}
+		}
 	}
 }
